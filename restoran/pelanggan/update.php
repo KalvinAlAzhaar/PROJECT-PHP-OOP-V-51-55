2ea6d3835996d0e29a6d52ad9825f22e6ffc40ca
KalvinAlAzhaar <87465503+KalvinAlAzhaar@users.noreply.github.com>
2021-11-27 13:03:49 +0700

    Add files via upload

---
+++ restoran/pelanggan/update.php
@@ -0,0 +1,14 @@
+<?php 
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $row = $db->getItem("SELECT * FROM tblpelanggan WHERE idpelanggan=$id");
+        if ($row['aktif']==0) {
+            $aktif = 1;
+        }else {
+            $aktif = 0;
+        }
+        
+        $sql = "UPDATE tblpelanggan SET aktif=$aktif WHERE idpelanggan=$id";
+        $db->runSQL($sql);
+        header("location:?f=pelanggan&m=select");
+    }
